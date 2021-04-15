6c04bf4566c17f350b5dd31e97ff09b57541c94d
xiusl <xiushilin@hotmail.com>
2021-04-15 14:18:41 +0800

    api: add middleware test

--- api/server.go
+++ api/server.go
@@ -1,65 +1,69 @@
 package api
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator/v10"
-	db "github.com/xiusl/bank/db/sqlc"
-	"github.com/xiusl/bank/token"
-	"github.com/xiusl/bank/util"
+    "github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin/binding"
+    "github.com/go-playground/validator/v10"
+    db "github.com/xiusl/bank/db/sqlc"
+    "github.com/xiusl/bank/token"
+    "github.com/xiusl/bank/util"
 )
 
 // Server http 服务
 type Server struct {
-	store      db.Store
-	router     *gin.Engine
-	config     util.Config
-	tokenMaker token.Maker
+    store      db.Store
+    router     *gin.Engine
+    config     util.Config
+    tokenMaker token.Maker
 }
 
 // NewServer 创建一个新的服务，并设置路由
 func NewServer(config util.Config, store db.Store) (*Server, error) {
-	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmertricKey)
-	if err != nil {
-		return nil, fmt.Errorf("cannot create token maker: %w", err)
-	}
-	server := &Server{
-		config:     config,
-		store:      store,
-		tokenMaker: tokenMaker,
-	}
+    tokenMaker, err := token.NewPasetoMaker(config.TokenSymmertricKey)
+    if err != nil {
+        return nil, fmt.Errorf("cannot create token maker: %w", err)
+    }
+    server := &Server{
+        config:     config,
+        store:      store,
+        tokenMaker: tokenMaker,
+    }
 
-	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterValidation("currency", validCurrency)
-	}
+    if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+        v.RegisterValidation("currency", validCurrency)
+    }
 
-	server.setupRouter()
+    server.setupRouter()
 
-	return server, nil
+    return server, nil
 }
 
 func (server *Server) setupRouter() {
-	router := gin.Default()
+    router := gin.Default()
 
-	// 设置路由
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
-	router.POST("/transfers", server.createTransfer)
-	router.POST("/users", server.createUser)
-	router.POST("/users/login", server.loginUser)
+    // 设置路由
 
-	server.router = router
+    router.POST("/users", server.createUser)
+    router.POST("/users/login", server.loginUser)
+
+    authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+
+    authRoutes.POST("/accounts", server.createAccount)
+    authRoutes.GET("/accounts/:id", server.getAccount)
+    authRoutes.GET("/accounts", server.listAccount)
+    authRoutes.POST("/transfers", server.createTransfer)
+
+    server.router = router
 }
 
 // Start 开启服务器，address 监听的地址
 func (server *Server) Start(address string) error {
-	return server.router.Run(address)
+    return server.router.Run(address)
 }
 
 // 格式化错误信息
 func errorResponse(err error) gin.H {
-	return gin.H{"error": err.Error()}
+    return gin.H{"error": err.Error()}
 }
